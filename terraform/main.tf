915808a12515d84588712abadc15e9a60084d3f2
Faraz Khawaja <khwajafarazahmed@yahoo.com>
2023-10-22 21:49:48 +0500

    adding tf files + fixing ports

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = var.resource_group_name
+}
+
+
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = var.cluster_name
+  kubernetes_version  = "1.28.0"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name                = "system"
+    node_count          = var.agent_count
+    vm_size             = "Standard_DS2_v2"
+    type                = "VirtualMachineScaleSets"
+    enable_auto_scaling = false
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    load_balancer_sku = "basic"
+    network_plugin    = "kubenet" 
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,35 @@
+variable "agent_count" {
+  default = 3
+}
+
+# The following two variable declarations are placeholder references.
+# Set the values for these variable in terraform.tfvars
+variable "aks_service_principal_app_id" {
+  default = ""
+}
+
+variable "aks_service_principal_client_secret" {
+  default = ""
+}
+
+variable "admin_username" {
+  default = "demo"
+}
+
+variable "cluster_name" {
+  default = "k8s-cluster"
+}
+
+variable "dns_prefix" {
+  default = "demok8s"
+}
+
+variable "resource_group_location" {
+  default     = "West Europe"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name" {
+  default     = "demo-terraform-kubernetes-RG"
+  description = "Resource group name that is unique in your Azure subscription."
+}
